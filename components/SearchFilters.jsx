9191d8a02affd1107f37a91fc93fc0c145a2d036
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-12-11 18:07:56 +0500

    Query properties on Search Page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,14 @@
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+
+function SearchFilters() {
+    return (
+        <div>
+            Search Filter
+        </div>
+    )
+}
+
+export default SearchFilters
