d25795d6f7d4550767b4f0c90244b53cf6f42bf2
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-12-11 19:06:16 +0500

    add search filters

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bf01b1cade0b5636"; }
+/******/ 		__webpack_require__.h = function() { return "2b58d8c3fba44a47"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
